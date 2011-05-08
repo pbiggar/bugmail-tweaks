175405a839081aa32845888f060db8c97d62d47b
Paul Biggar <paul.biggar@gmail.com>
2011-05-08 09:02:06 -0700

    Add tests. We'll need to refactor a bit to make them work though.

---
+++ test/combine.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+"""Read the test files from this directory and combine them into a JSON file which can be read from JS tests."""
+
+import glob
+import json
+
+files = glob.glob("msg?")
+
+class Struct(object):
+  pass
+
+result = []
+for f in files:
+  # Read
+  contents = file(f).readlines();
+  assert len (contents) >= 2
+  
+  # Parse
+  tags = contents[0].rstrip().split(',')
+  contents = "\n".join(contents[1:])
+
+  # Create struct
+  result.append({"contents": contents, "tags": tags})
+
+
+# Write out JSON
+result = json.dumps(result)
+file("../data/test-msgs.js", "w").write(result);
+
+
--- test/test-main.js
+++ test/test-main.js
@@ -1,4 +1,5 @@
 const main = require("main");
+const self = require("self");
 
 exports.test_test_run = function(test) {
   test.pass("Unit test running!");
@@ -8,25 +9,22 @@ exports.test_id = function(test) {
   test.assert(require("self").id.length > 0);
 };
 
-exports.test_url = function(test) {
-  require("request").Request({
-    url: "http://www.mozilla.org/",
-    onComplete: function(response) {
-      test.assertEqual(response.statusText, "OK");
-      test.done();
-    }
-  }).get();
-  test.waitUntilDone(20000);
-};
+exports.test_thingy = function(test) {
 
-exports.test_open_tab = function(test) {
-  const tabs = require("tabs");
-  tabs.open({
-    url: "http://www.mozilla.org/",
-    onReady: function(tab) {
-      test.assertEqual(tab.url, "http://www.mozilla.org/");
-      test.done();
-    }
-  });
-  test.waitUntilDone(20000);
-};
+  // Access the code
+  require("
+
+  // Extract the tests
+  json = self.data.load("test-msgs.js");
+  tests = JSON.parse(json);
+
+  // Each tag names a function which should trigger a change in the contents
+  for (var i in tests) {
+    var contents = tests[i].contents;
+    var tags = data[i].tags;
+  }
+
+  test.assertEqual(5,5);
+}
+
+
