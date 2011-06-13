daeca9a28426de0be2a4639afacc9d1fb234e3c7
Paul Biggar <paul.biggar@gmail.com>
2011-06-12 21:25:15 -0700

    Refactor and comment testing.
    Test parsing for comment annotations.
    Check annotations are correct.

--- test/test-main.js
+++ test/test-main.js
@@ -1,6 +1,6 @@
 const _self = require("self");
 
-exports.test_thingy = function(test) {
+exports.test_bzmail = function(test) {
 
   // Extract the tests
   var json = _self.data.load("test-msgs.js");
@@ -15,11 +15,22 @@ exports.test_thingy = function(test) {
   // Each tag names a function which should trigger a change in the contents
   for each (var t in tests) {
     console.log(t.name);
+
+    // Check all of our messages are matched
     test.assert (tw.matches(t.html));
 
-    // Some aren't worth parsing, since they have no info, but let's parse useful ones.
-    if (t.html.indexOf("--- Comment") != -1) {
-      test.assert (tw.parse(t.html));
+    var obj = tw.parse(t.html);
+
+    // Check that we haven't mistagged
+    if (obj) {
+      for each (tag in ["comment"]) {
+        test.assert (tag in t.tags);
+      }
+    }
+
+    // Check comments
+    if ("comment" in t.tags) {
+      test.assert (obj);
     }
   }
 }
